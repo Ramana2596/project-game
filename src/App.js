7c80344fb774e0be8236059c426711949de4a712
Ramana <muthuramana96@gmail.com>
2024-09-17 01:25:22 +0530

    added project files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import GameNavigation from './components/GameNavigation';
+
+function App() {
+  return (
+    <div className="App">
+      <GameNavigation />
+    </div>
+  );
+}
+
+export default App;
