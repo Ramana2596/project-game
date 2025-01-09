5200a519a91ee540713bb9a9a3a5c6443b520807
Ramana <muthuramana96@gmail.com>
2025-01-09 23:54:47 +0530

    implemented account info button

--- src/pages/GameDashboard/GameDashboard.jsx
+++ src/pages/GameDashboard/GameDashboard.jsx
@@ -1,33 +1,53 @@
-import * as React from 'react';
-import { useEffect, useState } from 'react';
-import GenericTable from '../../components/GenericTable';
-import { getDashboardData } from './services/gameDashboard.js';
+import * as React from "react";
+import { useEffect, useState } from "react";
+import GenericTable from "../../components/GenericTable";
+import { getDashboardData } from "./services/gameDashboard.js";
+import { Typography } from "@mui/material";
 
 function GameDashboard() {
   const [tableData, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const tableHeadings = ['Game_Id', 'Game_Title', 'Game_Short_Title', 'Game_Objective', 'Discipline', 'Subject', 'Faculty', 'Duration_Hours', 'Max_Seats', 'Max_Sessions'];
+  const tableHeadings = [
+    "Game_Id",
+    "Game_Title",
+    "Game_Short_Title",
+    "Game_Objective",
+    "Discipline",
+    "Subject",
+    "Faculty",
+    "Duration_Hours",
+    "Max_Seats",
+    "Max_Sessions",
+  ];
 
   // let { apiResponse: gameIdData, apiFailureErrorRes: gameBatchFailureRes, isLoading: gameBatchIsLoading } = FetchDataFromApi('https://loving-humpback-monthly.ngrok-free.app/api/data', true);
 
   useEffect(() => {
-        getDashboardData()
-        .then((response) => {
-          if(response) {
-            setData(response.data);
-          }
-        })
-        .catch((err) => setError(err?.message))
-        .finally(() => setLoading(false));
+    getDashboardData()
+      .then((response) => {
+        if (response) {
+          setData(response.data);
+        }
+      })
+      .catch((err) => setError(err?.message))
+      .finally(() => setLoading(false));
   }, []);
 
-  if (loading) return (<div>...Loading</div>);
-  if (error) return (<div>...Error</div>);
+  if (loading) return <div>...Loading</div>;
+  if (error) return <div>...Error</div>;
 
   return (
-    <GenericTable inputTableHeadings={tableHeadings} inputTableData={tableData}></GenericTable>
+    <div>
+      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Welcome to Operation Game
+      </Typography>
+      <GenericTable
+        inputTableHeadings={tableHeadings}
+        inputTableData={tableData}
+      ></GenericTable>
+    </div>
   );
 }
 
--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from "react";
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
-import IconButton from "@mui/material/IconButton";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+  Button,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
 import GameNavigationMenu from "../GameNavigationMenu";
 import GameMaster from "../GameMaster";
 import GameBatch from "../GameBatch";
@@ -21,7 +30,8 @@ import IncomeStatementInfo from "../IncomeStatementInfo/IncomeStatementInfo.jsx"
 
 export default function Index() {
   const [open, setOpen] = useState(false);
-  const { user, setAccessablePageIds } = useUser();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { user, userInfo, setAccessablePageIds } = useUser();
 
   useEffect(() => {
     if (user?.role) {
@@ -39,6 +49,14 @@ export default function Index() {
     setOpen(newOpen);
   };
 
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar sx={{ marginBottom: 5 }} position="static">
@@ -53,10 +71,68 @@ export default function Index() {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Welcome to Operation Game
-          </Typography>
-          <SignOutButton></SignOutButton>
+          <div style={{ marginLeft: "auto" }}>
+            <Button
+              onClick={handleMenu}
+              color="inherit"
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                textTransform: "none",
+                border: "1px solid",
+                borderRadius: "30px",
+                padding: "5px 20px",
+                backgroundColor: "rgba(255, 255, 255, 0.1)",
+                width: "250px",
+              }}
+            >
+              <Typography variant="h6" component="div" sx={{ mr: 1 }}>
+                {user?.role}
+              </Typography>
+              <AccountCircle />
+            </Button>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+              sx={{
+                "& .MuiPaper-root": {
+                  borderRadius: "10px",
+                  padding: "10px",
+                  width: "250px",
+                  backgroundColor: "rgba(255, 255, 255, 0.6)",
+                },
+              }}
+            >
+              <MenuItem disabled>{userInfo?.loginId}</MenuItem>
+              <MenuItem
+                onClick={handleClose}
+                sx={{
+                  "& .MuiButton-root": {
+                    width: "100%",
+                    justifyContent: "flex-start",
+                    textTransform: "none",
+                    padding: "10px",
+                  },
+                  "&:hover": {
+                    backgroundColor: "rgba(255, 255, 255, 0.2)",
+                  },
+                }}
+              >
+                <SignOutButton />
+              </MenuItem>
+            </Menu>
+          </div>
         </Toolbar>
       </AppBar>
       <GameNavigationMenu openState={open} toggleDrawer={toggleDrawer} />
