6dfe3a84811c20b40c20168f5ef8f10cdfd460b0
SivaGita <sivagi.mu@gmail.com>
2025-04-11 20:09:45 +0530

    "referenceICon picture adeed in assets"

--- src/pages/ValueStreamMap/services/service.js
+++ src/pages/ValueStreamMap/services/service.js
@@ -1,7 +1,7 @@
 import api from '../../../core/interceptor/api-interceptor';
 
-export function getShopPerformanceInfo(queryParams) {
-    return api.get('/api/getShopPerformanceInfo',
+export function getValueStreamMap(queryParams) {
+    return api.get('/api/getValueStreamMap',
         {
             params: { ...queryParams }
         });
