dcbaf0fa678db906e2fc2710f53881680551efb4
SivaGita <sivagi.mu@gmail.com>
2025-04-13 10:23:38 +0530

    "Used right filenaming convention, by its function. ~ SalesRecordInfo is ~AccountPayable"

--- src/pages/AccountPayable/services/service.js
+++ src/pages/AccountPayable/services/service.js
@@ -1,6 +1,6 @@
 import api from '../../../core/interceptor/api-interceptor';
 
-export function getSalesRecordInfo(queryParams) {
+    export function getAccountPayable(queryParams) {
     return api.get('/api/getAccountPayableData',
         {
             params: { ...queryParams }
