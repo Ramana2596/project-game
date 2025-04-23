6c4e54f23dce4fc9752348ed773b8e3d5d1c133b
SivaGita <sivagi.mu@gmail.com>
2025-04-23 20:56:31 +0530

    "Changes in Heading & Text info"

--- src/pages/FeatureManagement/constants/pageConstants.js
+++ src/pages/FeatureManagement/constants/pageConstants.js
@@ -3,7 +3,7 @@ import GameTeamPlay from "../components/GameTeamPlay";
 export const pageConstants = {
     accordianList: [
         {
-            headerValue: 'Play Action in Simulation Game',
+            headerValue: 'Operation Management- Simulation Game',
             contentValue: <GameTeamPlay />,
             isExpanded: false
         }
@@ -11,6 +11,6 @@ export const pageConstants = {
     gameFlagButtonLabel: 'Flag-Off',
     gameTeamPlayButtonLabel: 'Play',
     gameTeamPlayRollBack: 'Revert Back',
-    teamPlayConfirmation: 'To Step through on your Current Decision,     Click =>',
+    teamPlayConfirmation: 'To ACT on your Current Input,     Click =>',
     teamPlayRollBackConfirmation: 'To Revert back to Previous Period,      Click =>',
 }
