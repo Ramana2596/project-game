a0204a84176dcd0b7b2996b8a32b559ef8fdd3cf
SivaGita <sivagi.mu@gmail.com>
2025-05-01 18:32:02 +0530

    Renamed  Page from GameInSession to  ClassRoomSession"

---
+++ src/pages/ClassRoomSession/constants/pageConstants.js
@@ -0,0 +1,13 @@
+export const pageConstants = {
+//  gameBatch: "Batch",
+//  gameTeam: "Team",
+  tableHeading: [
+    "Team",
+    "Period",
+    "Completed Stage",
+    "Role in Team",
+    "User Name",
+    "In Session",
+    "Attending As",
+  ]
+};
