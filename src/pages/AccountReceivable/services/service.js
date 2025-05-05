628e54ced246c63c47c6622a06d5976158bb6175
SivaGita <sivagi.mu@gmail.com>
2025-05-05 10:54:08 +0530

    "AcReceivable is up & okay. So , AccountReceivable is deleted"

--- src/pages/AccountReceivable/services/service.js
+++
@@ -1,8 +0,0 @@
-import api from '../../../core/interceptor/api-interceptor';
-
-export function getAccountReceivable(queryParams) {
-    return api.get('/api/getAccountReceivable',
-        {
-            params: { ...queryParams }
-        });
-}
