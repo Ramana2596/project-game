099e9bf3580f0e94f07a6b828b3b9e0d95b3cc44
SivaGita <sivagi.mu@gmail.com>
2025-05-30 15:27:54 +0530

    File renamed StdMarketInput. So file in earlier filename get deleted

--- src/pages/ReleaseMarketInput/constants/pageConstants.js
+++
@@ -1,7 +0,0 @@
-export const pageConstants = {
- gameBatch: "Game Batch",
- // gameTeam: "Game Team",
-  tableHeading: [
-    "Release Standard Market Input for Current Period"
-  ]
-};
--- src/pages/ReleaseMarketInput/services/service.js
+++
@@ -1,8 +0,0 @@
-import api from '../../../core/interceptor/api-interceptor';
-
-export function getReleaseMarketInput(queryParams) {
-    return api.get('/api/getReleaseMarketInput',
-        {
-            params: { ...queryParams }
-        });
-}
