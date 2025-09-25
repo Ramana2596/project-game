3588355e1753e3c206ce1d1fefe57076bd88514b
SivaGita <sivagi.mu@gmail.com>
2025-09-25 11:18:40 +0530

    UI IncomeStatmentInfo
    Commented old lines deleted.

--- src/pages/IncomeStatementInfo/constants/pageConstants.js
+++ src/pages/IncomeStatementInfo/constants/pageConstants.js
@@ -1,26 +1,3 @@
-/*
-export const pageConstants = {
-  gameBatch: "Batch",
-  gameTeam: "Team",
-  tableHeading: [
-    "Line_no",
-    "Details",
-    "PrvYear_End",
-    "Period_1",
-    "Period_2",
-    "Period_3",
-    "Period_4",
-    "Period_5",
-    "Period_6",
-    "Period_7",
-    "Period_8",
-    "Period_9",
-    "Period_10",
-    "Period_11",
-    "Period_12",
-  ],
-};
-*/
 export const pageConstants = {
   gameBatch: "Batch",
   gameTeam: "Team",
