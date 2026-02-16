c3a63868cc823647b53d1c2b1fe97db94e46bff7
SivaGita <sivagi.mu@gmail.com>
2026-02-16 20:47:57 +0530

    Formatted API Service for ease of readability

--- src/pages/LaunchStrategy/services/strategyLaunchedService.js
+++ src/pages/LaunchStrategy/services/strategyLaunchedService.js
@@ -1,21 +1,49 @@
 import api from '../../../core/interceptor/api-interceptor';
 
 export function getGameId(queryParams) {
-    return api.get('/api/getStrategyLaunch', { params: { ...queryParams, cmdLine: "launchData" } });
+    return api.get('/api/getStrategyLaunch',
+        {
+            params: {
+                ...queryParams,
+                cmdLine: "launchData"
+            }
+        });
 }
 
 export function getStrategySetData(queryParams) {
-    return api.get('/api/getStrategyLaunch', { params: { ...queryParams, cmdLine: "Launch_Strategy_Set" } });
+    return api.get('/api/getStrategyLaunch',
+        {
+            params: {
+                ...queryParams,
+                cmdLine: "Launch_Strategy_Set"
+            }
+        });
 }
 
 export function getGameBatchData(queryParams) {
-    return api.get('/api/getStrategyLaunch', { params: { ...queryParams, cmdLine: "Get_Batch" } });
+    return api.get('/api/getStrategyLaunch',
+        {
+            params: {
+                ...queryParams,
+                cmdLine: "Get_Batch"
+            }
+        });
 }
 
 export function getStrategySetNoDataApi(queryParams) {
-    return api.get('/api/getStrategyLaunch', { params: { ...queryParams, cmdLine: "Get_Strategy" } });
+    return api.get('/api/getStrategyLaunch',
+        {
+            params: {
+                ...queryParams,
+                cmdLine: "Get_Strategy"
+            }
+        });
 }
 
 export function updateStrategyLaunchData(queryParams) {
-    return api.post('/api/updateStrategyLaunch', { ...queryParams, cmdLine: "Add_Strategy" });
+    return api.post('/api/updateStrategyLaunch',
+        {
+            ...queryParams,
+            cmdLine: "Add_Strategy"
+        });
 }
