d2fb1879f55f352fda146d39de8fa59b0a1ce486
SivaGita <sivagi.mu@gmail.com>
2026-02-27 18:04:38 +0530

    UI BOMInfo - pageConstants
    Changes
    Column Heading
    Added Hidden Columns

--- src/pages/BOMInfo/constants/pageConstants.js
+++ src/pages/BOMInfo/constants/pageConstants.js
@@ -2,11 +2,21 @@ export const pageConstants = {
 //  gameBatch: "Batch",
 //  gameTeam: "Team",
   tableHeading: [
-    "Team",
-    "Part",
+  //  "Team",
+    "Raw Material",
     "UOM",
     "Quantity Per Set",
     "Used in Product",
-    "Doc Issue Level"
+  //  "Doc Issue Level"
+  ],
+   hiddenColumns: [
+    "Game_Id",
+    "Game_Batch",
+    "Game_Team",
+    "Team",
+    "Part_No",
+    "Used_in_Product",
+    "Issue_Level",
+
   ]
 };
