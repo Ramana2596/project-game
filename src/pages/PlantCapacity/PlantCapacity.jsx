0f120daf90d785bec759bac16178e6be8e00a817
SivaGita <sivagi.mu@gmail.com>
2026-09-03 18:35:54 +0530

    UI_Plant_Capacity
    Added PlantCapacity UI 22 060

--- src/pages/PlantCapacity/PlantCapacity.jsx
+++ src/pages/PlantCapacity/PlantCapacity.jsx
@@ -1,38 +1,53 @@
+// PlantCapacity.jsx
+// Work_Centre and Plant Capacity, load, Utilisation
+
 import { Box } from "@mui/material";
-import Grid from "@mui/material/Grid2";
 import GenericTable from "../../components/GenericTable.jsx";
 import { useUser } from "../../core/access/userContext.jsx";
 import { useEffect, useState } from "react";
-import { getPlantCapacity} from "./services/service.js";
+import { getPlantCapacity } from "./services/service.js";
 import { pageConstants } from "./constants/pageConstants.js";
 
 export default function PlantCapacity() {
   const { userInfo } = useUser();
-  let payload = {
+
+  const payload = {
     gameId: userInfo?.gameId,
     gameBatch: userInfo?.gameBatch,
     gameTeam: userInfo?.gameTeam,
   };
-  const [tableData, setTableData] = useState([]);
+
+  const [workCentreData, setWorkCentreData] = useState([]);
+  const [plantData, setPlantData] = useState([]);
+
   useEffect(() => {
     getPlantCapacity(payload).then((response) => {
-      if (response) {
-        setTableData(response.data);
+      if (response?.data) {
+        setWorkCentreData(response.data.Work_Centre || []);
+        setPlantData(response.data.Plant || []);
       }
     });
   }, []);
 
   return (
     <Box sx={{ flexGrow: 1 }}>
- 
+
+      {/* Plant Summary - will be used for UX cards */}
+      {plantData.length > 0 && (
+        <Box>
+          {/* Plant summary cards will come here */}
+        </Box>
+      )}
+
+      {/* Work Centre / Machine Utilisation */}
       <GenericTable
         inputTableHeadings={pageConstants.tableHeading}
-        inputTableData={tableData}
+        inputTableData={workCentreData}
         ifNoData={null}
         highlightColumnsByField={pageConstants.highlightedColumns}
         hiddenColumns={pageConstants.hiddenColumns}
-      ></GenericTable>
-      
+      />
+
     </Box>
   );
 }
